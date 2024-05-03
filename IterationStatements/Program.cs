2e214d3cd171ae15f3991c2028cb4f0cf25b09bc
Brooks Stockley <bstockley26@gmail.com>
2024-05-03 11:20:45 -0400

    finished all lukewarm sections

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -3,9 +3,9 @@
     public class Program
     {
         //LukeWarm Section: Create methods below
-        //Write a method that will print to the console all numbers 1000 through - 1000
+        
 
-        //Write a method that will print to the console numbers 3 through 999 by 3 each time
+         
 
         //Write a method to accept two integers as parameterss and check whether they are equal or not
         
@@ -27,7 +27,101 @@
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-            
+            VotingElegibity();
+        }
+        static void ListThousand()
+        {
+            var numbers = new List<int>();
+            int num;
+            num = 10;
+
+            do
+            {
+                Console.WriteLine(num);
+                --num;
+                numbers.Add(num);
+                
+            } while (num >= -10);
+
+        }
+        static void AddByThree()
+        {
+            var numbers = new List<int>();
+            int num;
+            num = 0;
+
+            do
+            {
+                Console.WriteLine(num);
+                num += 3;
+                numbers.Add(num);
+
+            } while (num <= 999);
+        }
+        static void EqualityCheck()
+        {
+            int a;
+            int b;
+
+            a = 8;
+            b = 8;
+
+            var correctStatement = (a == b) ? "Integars are equal" : "Integers are not equal!";
+
+            Console.WriteLine(correctStatement);
+        }
+        static void EvenOddTest()
+        {
+            int a;
+            int division;
+            bool evenOrOdd;
+
+            a = 7;
+            division = a % 2;
+
+            evenOrOdd = division == 0;
+
+            if (evenOrOdd == true)
+            {
+                Console.WriteLine($"{a} is even");
+            }
+            else
+            {
+                Console.WriteLine($"{a} is odd!");
+            }
+        }
+        static void PositiveOrNegative()
+        {
+            int a;           
+            bool posOrNeg;
+
+            a = -8;
+            posOrNeg = a >= 0;
+
+            if (posOrNeg == true)
+            {
+                Console.WriteLine($"{a} is a positive number!");
+            }
+            else
+            {
+                Console.WriteLine($"{a} is a negative number");
+            }
+        }
+        static void VotingElegibity()
+        {
+            int age;
+
+            Console.WriteLine("What is your age?");
+            age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You are elegibly to vote!!");
+            }
+            else
+            {
+                Console.WriteLine("You are to young to vote");
+            }
         }
     }
 }
